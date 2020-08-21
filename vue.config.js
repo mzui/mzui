dd82b2148282d26da32b64bb25f390f6294f2a28
leo <cwq0312@163.com>
2020-08-21 14:40:54 +0800

    add

---
+++ vue.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+module.exports = {
+  //lintOnSave: true,
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set("vue$", "vue/dist/vue.esm.js")
+      .set("@", resolve("src"))
+      .set("@assets", resolve("src/assets"))
+      .set("@coms", resolve("src/components"))
+  },
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true, //less 配置
+      },
+    },
+  },
+  devServer: {
+    port: "4000",
+    proxy: {
+      "/api": {
+        target: "http://localhost:8081",//"http://47.91.221.11:8081",
+        ws: true,
+        changeOrigin: true,
+      },
+      "/foo": {
+        target: "<other_url>",
+      },
+    },
+  },
+};
